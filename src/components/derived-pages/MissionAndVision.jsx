38aa4159313a727e364807b50f9e7390a269009e
Shubham228197 <shubham228pandey@gmail.com>
2024-07-17 01:31:45 +0530

    files imported

--- src/components/derived-pages/MissionAndVision.jsx
+++ src/components/derived-pages/MissionAndVision.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import ItServiceInfoPage from "../branch-pages/ItServiceInfoPage";
+import CloudMigrationInfoPage from "../branch-pages/CloudMigrationInfoPage";
+import TransformativeTechInfoPage from "../branch-pages/TransformativeTechInfoPage";
+import TrainingOfferingInfoPage from "../branch-pages/TrainingOfferingInfoPage";
+import { Box } from "@mui/material";
 
 export default function MissionAndVission() {
-  return <div></div>;
+  return (
+    <Box>
+      <ItServiceInfoPage />
+      <CloudMigrationInfoPage />
+      <TransformativeTechInfoPage />
+      <TrainingOfferingInfoPage />
+    </Box>
+  );
 }
