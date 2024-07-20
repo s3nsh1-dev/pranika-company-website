cabd14e672d2975eaef6991f65f1848a6a33e7fe
Shubham228197 <shubham228pandey@gmail.com>
2024-07-20 18:47:56 +0530

    screen Title change

---
+++ src/components/landing-page/NavbarPrimaryContent.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import plogo from "../../images/p-bg-less-copy.png";
+import { Link } from "react-router-dom";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+export default function NavbarPrimaryContent() {
+  const theme = useTheme();
+  const isXSOrLess = useMediaQuery(theme.breakpoints.down("sm"));
+  const largeScreenTitle = "Pranika Technologies and Consulting Pvt. Ltd.";
+  const smallScreenTitle = "Pranika Technologies";
+
+  return (
+    <Box
+      component={Link}
+      to='/'
+      sx={{
+        textDecoration: "none",
+        color: "inherit",
+        flexGrow: 1,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <img
+          src={plogo}
+          alt='pranika-logo'
+          style={{
+            height: "30px",
+            width: "30px",
+            borderRadius: "50px",
+            margin: "0px 10px 0px 30px",
+          }}
+        />
+        <Typography fontSize={`1.4em`}>
+          {isXSOrLess ? smallScreenTitle : largeScreenTitle}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
