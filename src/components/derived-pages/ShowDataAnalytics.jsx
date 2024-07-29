af41cd45de6452efe7e0785edf1180d8713f7659
Shubham228197 <shubham228pandey@gmail.com>
2024-07-29 22:04:46 +0530

    Link added for D.A.

--- src/components/derived-pages/ShowDataAnalytics.jsx
+++ src/components/derived-pages/ShowDataAnalytics.jsx
@@ -3,6 +3,8 @@ import ShowServiceSubTitle from "../branch-pages/ShowServiceSubTitle";
 import ShowServiceDataPoints from "../branch-pages/ShowServiceDataPoints";
 import ShowDataAnalyticsApplication from "../branch-pages/ShowDataAnalyticsApplication";
 import WhyChooseDataAnalytics from "../branch-pages/WhyChooseDataAnalytics";
+import SecondryNavbar from "../home-page/SecondryNavbar";
+import PrimaryFooter from "../home-page/PrimaryFooter";
 
 const title = "Data Analytics";
 const subtitle =
@@ -11,10 +13,12 @@ const subtitle =
 export default function ShowDataAnalytics() {
   return (
     <>
+      <SecondryNavbar />
       <ShowServiceSubTitle title={title} subtitle={subtitle} />
       <ShowServiceDataPoints />
       <WhyChooseDataAnalytics />
       <ShowDataAnalyticsApplication />
+      <PrimaryFooter />
     </>
   );
 }
