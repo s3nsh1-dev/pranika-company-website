626d658c04d6fbc05b790ca8d99376626da779f5
Shubham228197 <shubham228pandey@gmail.com>
2024-08-03 23:57:41 +0530

    transformation footer text filled

---
+++ src/components/branch-pages/CallToActionTransformation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function CallToActionTransformation() {
+  return (
+    <Box>
+      <Typography>Call to Action</Typography>
+      <Typography>
+        Ready to unlock the full potential of your data? Contact us today for a
+        free consultation and discover how we can transform your data into a
+        strategic asset
+      </Typography>
+      <Typography>
+        Contact Us: (+91) 9151391112 | info@pranikatech.com
+      </Typography>
+      <Typography>
+        Follow us on our Social Media Channel to stay UpToDate
+      </Typography>
+    </Box>
+  );
+}
