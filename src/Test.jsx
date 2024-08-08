f2ca18616a2a657238d299aeccd9ec35f51f358f
Shubham228197 <shubham228pandey@gmail.com>
2024-08-08 23:22:55 +0530

    Reset Text

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,13 +1,5 @@
 import React from "react";
-import { useTheme } from "@mui/material/styles";
 
-export default function PrimaryColorHex() {
-  const theme = useTheme();
-  const primaryColorHex = theme.palette.primary.main;
-
-  return (
-    <div>
-      <p>Primary Color Hex: {primaryColorHex}</p>
-    </div>
-  );
+export default function Test() {
+  return <div></div>;
 }
