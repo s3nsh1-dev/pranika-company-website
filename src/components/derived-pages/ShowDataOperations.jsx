a27f91524ec110b450d8fe3f39efd3e031142d55
Shubham228197 <shubham228pandey@gmail.com>
2024-08-11 22:48:47 +0530

    data operations for constants

--- src/components/derived-pages/ShowDataOperations.jsx
+++ src/components/derived-pages/ShowDataOperations.jsx
@@ -8,99 +8,16 @@ import SecondryNavbar from "../home-page/SecondryNavbar";
 import PrimaryFooter from "../home-page/PrimaryFooter";
 import coverImage from "../../images/data-operations-cover.jpg";
 import transformationImage from "../../images/data-transformation.jpg";
-
-const title = "Data Operations";
-const subtitle = "Unlock Efficiency, Accuracy, and Insights with us ";
-
-const pointsTitle = "Why Choose Us for Your Data Operations?";
-const contentPoints = [
-  {
-    id: 0,
-    primary: "Experienced Professionals:",
-    secondary:
-      "Our team of seasoned data engineers and analysts brings years of experience in managing complex data operations across various industries.",
-  },
-  {
-    id: 1,
-    primary: "Cutting-Edge Technology",
-    secondary:
-      "We leverage the latest tools and technologies to streamline your data processes, ensuring high performance and reliability.",
-  },
-  {
-    id: 2,
-    primary: "Tailored Solutions",
-    secondary:
-      "We understand that every business is unique. Our solutions are customized to meet your specific data needs and objectives.",
-  },
-  {
-    id: 3,
-    primary: "Proven Track Record",
-    secondary:
-      "Our portfolio of successful projects and satisfied clients speaks to our ability to deliver results that exceed expectations.",
-  },
-  {
-    id: 4,
-    primary: "Data Quality Management",
-    secondary:
-      "Ensure data accuracy, consistency, and completeness with our robust data quality solutions.",
-  },
-  {
-    id: 8,
-    primary: "Data Security",
-    secondary:
-      "Protect your sensitive data with our advanced security measures and protocols.",
-  },
-];
-
-const pageTitle =
-  "Specializing in Optimizing Data Operations to Harness the Full Potential of Your Data";
-const pageStartingContent =
-  "At Pranika Technologies and Consulting Pvt. Ltd., we specialize in optimizing data operations to help businesses harness the full potential of their data. Our comprehensive services ensure that your data processes are efficient, accurate, and scalable, providing you with actionable insights to drive informed decision-making.";
-
-const accordianDataTitle = "Unlock the Full Potential of Your Data";
-const accordianData = [
-  {
-    id: 0,
-    primary: "Enhanced Efficiency",
-    secondary: "Streamline your data processes to save time and resources.",
-  },
-  {
-    id: 1,
-    primary: "Accuracy",
-    secondary:
-      "Minimize errors and ensure reliable data for better decision-making.",
-  },
-  {
-    id: 3,
-    primary: "Insights",
-    secondary:
-      "Transform your data into valuable insights that drive strategic initiatives.",
-  },
-  {
-    id: 2,
-    primary: "Scalability",
-    secondary:
-      "Adapt to growing data volumes and business needs with our scalable solutions.",
-  },
-  {
-    id: 4,
-    primary: "Regulatory Compliance",
-    secondary:
-      "Stay compliant with industry standards and regulations with our comprehensive governance and security measures.",
-  },
-  {
-    id: 5,
-    primary: "Data Governance",
-    secondary:
-      "Implement policies and procedures to manage data assets effectively and ensure compliance with regulatory standards.",
-  },
-  {
-    id: 6,
-    primary: "Data Integration",
-    secondary:
-      "Seamlessly integrate data from multiple sources to create a unified, accessible data ecosystem.",
-  },
-];
+import {
+  title,
+  subtitle,
+  pointsTitle,
+  contentPoints,
+  pageTitle,
+  pageStartingContent,
+  accordianDataTitle,
+  accordianData,
+} from "../../constants/DataOperationsPage";
 
 export default function ShowDataOperations() {
   return (
