3907810e9f8bbe42a7ecf3d88bdb4b385ec8a4b7
Shubham228197 <shubham228pandey@gmail.com>
2024-08-11 23:00:05 +0530

    aboutUs contant

--- src/components/branch-pages/AboutUs.jsx
+++ src/components/branch-pages/AboutUs.jsx
@@ -3,30 +3,11 @@ import { Box, Grid, Typography } from "@mui/material";
 import image from "../../images/building-nobg.png";
 import SecondryNavbar from "../home-page/SecondryNavbar";
 import PrimaryFooter from "../home-page/PrimaryFooter";
+import {
+  coreOfPTCPL,
+  aboutPranika,
+} from "../../constants/AboutPranikaWebsitePage";
 
-const aboutPranika =
-  "Pranika Technologies and Consulting Pvt. Ltd. is your trusted partner in providing cutting-edge IT consulting, seamless cloud migrations, transformative technology solutions, and comprehensive professional training. With the addition of the ISO 9000:2015 certification, we further solidify our commitment to excellence. Our team of skilled experts ensures that our processes align with global best practices, empowering businesses to navigate the ever-evolving digital landscape and achieve their goals.";
-
-const coreOfPTCPL = [
-  {
-    id: 0,
-    heading: "Our Mission",
-    details:
-      "At Pranika, our mission is to be a transformative force in the technology industry. We strive to deliver innovative solutions that empower our clients to thrive in the digital age. By leveraging our deep expertise and industry insights, we help businesses unlock new possibilities and achieve sustainable growth.",
-  },
-  {
-    id: 1,
-    heading: "Our Values",
-    details:
-      "Integrity, Innovation, Collaboration, and Excellence are the core values that guide our every action. We are committed to building long-lasting partnerships with our clients, fostering a culture of continuous learning, and driving positive change in the communities we serve.",
-  },
-  {
-    id: 2,
-    heading: "Our Team",
-    details:
-      "Pranika's strength lies in our diverse and talented team of IT professionals. Hailing from various backgrounds and disciplines, our experts bring a wealth of knowledge and experience to the table. Together, we work in sync to provide tailored solutions that address our clients' unique challenges and aspirations.",
-  },
-];
 export default function AboutUs() {
   const renderingCoreOfPTCPL = coreOfPTCPL.map((values) => {
     return (
