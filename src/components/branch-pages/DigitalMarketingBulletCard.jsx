4ec9e016ead621370c13f85edc8de6440d5c12c8
Shubham228197 <shubham228pandey@gmail.com>
2024-08-24 23:57:47 +0530

    code include image
    file includes image/partial testing

--- src/components/branch-pages/DigitalMarketingBulletCard.jsx
+++ src/components/branch-pages/DigitalMarketingBulletCard.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import styled from "styled-components";
+// import image from "../../images/introDM.png";
+// import image from "../../images/InfluenceMarketing.png";
+// import image from "../../images/CMupscale.png";
+// import image from "../../images/DM_SEOimage.png";
+// import image from "../../images/DM_SocialMediaMarketing.png";
+// import image from "../../images/DM_EmailMarketing.png";
+import image from "../../images/DM_PPC.png";
 
 const BulletCardInfo = styled(Box)({
+  display: "flex",
+  flexWrap: "wrap",
   borderRadius: "20px",
   boxShadow: "2px 2px 5px 2px grey",
   margin: "10px",
@@ -18,18 +27,26 @@ const BulletCardInfo = styled(Box)({
 export default function DigitalMarketingBulletCard({ point }) {
   return (
     <BulletCardInfo>
-      <Typography fontWeight='bold' variant='h6'>
-        {point.title}
-      </Typography>
-      <Box component='ul'>
-        {point.content.map((bulletPoints, index) => {
-          return (
-            <Typography component='li' key={index}>
-              {bulletPoints}
-            </Typography>
-          );
-        })}
+      <Box>
+        <Typography fontWeight='bold' variant='h6'>
+          {point.title}
+        </Typography>
+        <Box component='ul'>
+          {point.content.map((bulletPoints, index) => {
+            return (
+              <Typography component='li' key={index}>
+                {bulletPoints}
+              </Typography>
+            );
+          })}
+        </Box>
       </Box>
+      <Box
+        component='img'
+        src={image}
+        alt='gghave'
+        sx={{ height: 200, width: 200, margin: "5px" }}
+      />
     </BulletCardInfo>
   );
 }
