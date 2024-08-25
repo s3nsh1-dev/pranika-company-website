2644f1846ce1e3d129e965de9eb46d2cc549d275
Shubham228197 <shubham228pandey@gmail.com>
2024-08-25 06:23:39 +0530

    New components for individual code

---
+++ src/components/branch-pages/TrainingPrgramFooter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function TrainingPrgramFooter() {
+  return (
+    <Typography
+      fontWeight='bold'
+      marginTop='10px'
+      textAlign='justify'
+      sx={{ margin: "3%" }}
+    >
+      Join us at Pranika Technologies and Consulting Pvt. Ltd., where we empower
+      individuals to thrive in the dynamic world of technology. Our expertly
+      crafted courses go beyond traditional education, offering hands-on
+      experience and industry-relevant skills that pave the way for a successful
+      career. Whether you're looking to upskill, transition into a new role, or
+      stay ahead of industry trends, our programs are designed to be your
+      gateway to a brighter and more prosperous future in the tech industry
+    </Typography>
+  );
+}
