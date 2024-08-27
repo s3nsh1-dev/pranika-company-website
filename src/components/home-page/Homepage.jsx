661a8d6aa25c8e28cc011e64e630caa71778176e
Shubham228197 <shubham228pandey@gmail.com>
2024-08-27 07:49:24 +0530

    added playlist on homepage

--- src/components/home-page/Homepage.jsx
+++ src/components/home-page/Homepage.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import SecondryNavbar from "./SecondryNavbar";
 import PrimaryFooter from "./PrimaryFooter";
 import AboutPranikaAccordion from "./AboutPranikaAccordian";
-import MeetOurTeam from "./MeetOurTeam";
+// import MeetOurTeam from "./MeetOurTeam";
 import HeroSection from "./HeroSection";
+import HomepageYoutubeSuggestions from "./HomepageYoutubeSuggestions";
 
 export default function Homepage() {
   return (
     <>
       <SecondryNavbar />
       <HeroSection />
+      <HomepageYoutubeSuggestions />
       <AboutPranikaAccordion />
       {/* <MeetOurTeam /> */}
       <PrimaryFooter />
