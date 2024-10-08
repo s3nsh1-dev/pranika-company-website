446c239e7439d0aa334bf8bf137b5367b6f163aa
Shubham228197 <shubham228pandey@gmail.com>
2024-10-08 18:40:45 +0530

    react-snap failed

---
+++ public/react-snap.config.js
@@ -0,0 +1,23 @@
+// react-snap.config.js
+module.exports = {
+  inlineCss: true,
+  puppeteerArgs: ["--no-sandbox", "--disable-setuid-sandbox"],
+  routes: [
+    "/",
+    "/home",
+    "/contact",
+    "/mission&vision",
+    "/teamsummary",
+    "/comingsoon",
+    "/about",
+    "/careers",
+    "/training",
+    "/data-analytics",
+    "/data-operations",
+    "/digital-marketing",
+    "/job-support-india",
+    "/data-transformation",
+    "/consult-recruit",
+    "/test",
+  ],
+};
