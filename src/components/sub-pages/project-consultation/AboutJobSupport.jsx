13ea6323c0dd04a8dd10b9d7ad7be07cfc07e30c
Shubham228197 <shubham228pandey@gmail.com>
2024-10-11 16:15:58 +0530

    code sanity

--- src/components/sub-pages/project-consultation/AboutJobSupport.jsx
+++ src/components/sub-pages/project-consultation/AboutJobSupport.jsx
@@ -2,47 +2,41 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { whyChooseJobSupportFormUs } from "../../../constants/ProjectConsultationConstants";
 
+const bulletPointStyle = {
+  margin: "10px 0px",
+  fontSize: "15px",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  padding: "10px 15px",
+  borderRadius: "5px",
+  border: "1px solid white",
+};
+const containerStyle = {
+  margin: "20px 3%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+const listTitleStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.35)",
+  padding: "10px 15px",
+  borderRadius: "5px",
+  border: "1px solid white",
+  margin: "2px",
+};
+
 export default function AboutJobSupport() {
   const renderWhyPoints = whyChooseJobSupportFormUs.map((point) => {
     return (
-      <Typography
-        component='li'
-        key={point.id}
-        sx={{
-          margin: "10px 0px",
-          fontSize: "15px",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          padding: "10px 15px",
-          borderRadius: "5px",
-          border: "1px solid white",
-        }}
-      >
+      <Typography component='li' key={point.id} sx={bulletPointStyle}>
         <span style={{ fontWeight: "bold" }}>{point.title}</span>:{" "}
         {point.content}
       </Typography>
     );
   });
   return (
-    <Box
-      sx={{
-        margin: "20px 3%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Typography
-        variant='h5'
-        textAlign='center'
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, 0.35)",
-          padding: "10px 15px",
-          borderRadius: "5px",
-          border: "1px solid white",
-          margin: "2px",
-        }}
-      >
+    <Box sx={containerStyle}>
+      <Typography variant='h5' textAlign='center' sx={listTitleStyle}>
         Need Help with Your Project or Job Role?
       </Typography>
       <Typography>
