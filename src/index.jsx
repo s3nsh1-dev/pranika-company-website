00d725fb0a9c6073716d0977bb14374418d6f6e4
Shubham228197 <shubham228pandey@gmail.com>
2024-10-13 23:07:42 +0530

    Adding Tag manager CODE

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,15 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./custom-hooks/theme";
 import App from "./App";
+import TagManager from "react-gtm-module";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-HZS6DQWBQN");
+const tagManagerArgs = {
+  gtmID: "GTM-XXXXXXX",
+};
 
+TagManager.initialize(tagManagerArgs);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
