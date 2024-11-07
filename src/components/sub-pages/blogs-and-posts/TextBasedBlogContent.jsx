46c0a6a3848c84da2e69c6024105e96134f1a1a3
Shubham228197 <shubham228pandey@gmail.com>
2024-11-07 23:53:18 +0530

    initial UI Done

--- src/components/sub-pages/blogs-and-posts/TextBasedBlogContent.jsx
+++ src/components/sub-pages/blogs-and-posts/TextBasedBlogContent.jsx
@@ -1,3 +1,4 @@
+import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
 
 export default function TextBasedBlogContent({
@@ -8,23 +9,41 @@ export default function TextBasedBlogContent({
 }) {
   const renderBulletPoints = bulletPoints.map((bullet) => {
     return (
-      <div key={bullet.id}>
-        <div>{bullet.pointsTitle}</div>
+      <Box key={bullet.id} margin={2}>
+        <Typography variant='h6' fontWeight='bold'>
+          {bullet.pointsTitle}
+        </Typography>
         {bullet.points.map((point, index) => {
-          return <div key={index}>{point}</div>;
+          return (
+            <Box component='li' key={index}>
+              {point}
+            </Box>
+          );
         })}
-      </div>
+      </Box>
     );
   });
   return (
-    <div>
-      <div>{title}</div>
-      <div>{intro}</div>
-      <div>
-        <div>{whyLearn.title}</div>
-        <div>{whyLearn.describes}</div>
-      </div>
-      <div>{renderBulletPoints}</div>
-    </div>
+    <Paper sx={{ margin: "20px", padding: "20px" }} elevation={6}>
+      <Typography
+        variant='h5'
+        sx={{
+          fontWeight: "bold",
+          textAlign: "center",
+          padding: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography textAlign='justify'>{intro}</Typography>
+      <Box margin={2}>
+        <Typography variant='h6' fontWeight='bold'>
+          {whyLearn.title}
+        </Typography>
+        <Typography textAlign='justify'>{whyLearn.describes}</Typography>
+      </Box>
+      <Box component='ul'>{renderBulletPoints}</Box>
+    </Paper>
   );
 }
