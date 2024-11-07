b89975e0bd7ea15f27e77c8a6254b67e036e8166
Shubham228197 <shubham228pandey@gmail.com>
2024-11-07 22:24:53 +0530

    fixes

--- src/components/sub-pages/blogs-and-posts/BlogsAndPosts.jsx
+++ src/components/sub-pages/blogs-and-posts/BlogsAndPosts.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import SecondryNavbar from "../../home-page/SecondryNavbar";
+import PrimaryFooter from "../../home-page/PrimaryFooter";
 
 export default function BlogsAndPosts() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <SecondryNavbar />
+      <PrimaryFooter />
+    </>
+  );
 }
