1fd66583c1d69b35fa14b87f4e1758754219e2aa
Shubham228197 <shubham228pandey@gmail.com>
2024-11-07 22:51:13 +0530

    constant

---
+++ src/constants/BlogsAndPostsConstants.js
@@ -0,0 +1,9 @@
+export const textBasedContent = [
+  {
+    id: 0,
+    primaryTitle: "Getting Started with Data Analytics: Key Concepts and Tools",
+    introduction:
+      "Data analytics is transforming the way businesses make decisions by providing insights from raw data. In this article, we’ll explore the basics of data analytics, popular tools, and how you can start building data-driven skills.",
+    content: "there",
+  },
+];
