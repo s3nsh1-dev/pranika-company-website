8ace6061f3ed6c928fbd443054f98a5fd2fa5c80
Shubham228197 <shubham228pandey@gmail.com>
2024-11-27 10:48:34 +0530

    Tab System

--- src/components/home-page/portfolio-page/Portfolio.jsx
+++ src/components/home-page/portfolio-page/Portfolio.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { Box, Button } from "@mui/material";
 import PortfolioAbout from "./PortfolioAbout";
 import PortfolioCaseStudy from "./PortfolioCaseStudy";
 import PortfolioWhyChooseUs from "./PortfolioWhyChooseUs";
@@ -6,13 +7,26 @@ import PortfolioTechStack from "./PortfolioTechStack";
 import PortfolioServices from "./PortfolioServices";
 
 const Portfolio = () => {
+  const [activeTab, setActiveTab] = useState("about");
+  const handleActiveTab = (newTab) => {
+    setActiveTab(newTab);
+  };
   return (
     <>
-      <PortfolioAbout />
-      <PortfolioServices />
-      <PortfolioWhyChooseUs />
-      <PortfolioTechStack />
-      <PortfolioCaseStudy />
+      <Box>
+        <Button onClick={() => handleActiveTab("about")}>Aim</Button>
+        <Button onClick={() => handleActiveTab("services")}>Services</Button>
+        <Button onClick={() => handleActiveTab("case")}>Case Study</Button>
+      </Box>
+      {activeTab === "about" && (
+        <Box>
+          <PortfolioAbout />
+          <PortfolioWhyChooseUs />
+          <PortfolioTechStack />
+        </Box>
+      )}
+      {activeTab === "services" && <PortfolioServices />}
+      {activeTab === "case" && <PortfolioCaseStudy />}
     </>
   );
 };
