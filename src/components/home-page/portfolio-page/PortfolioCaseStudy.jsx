dfbad1671262a206624b9fb6ab5a77cc0dd20090
Shubham228197 <shubham228pandey@gmail.com>
2024-11-27 14:49:52 +0530

    noCSS caseStudy comp

--- src/components/home-page/portfolio-page/PortfolioCaseStudy.jsx
+++ src/components/home-page/portfolio-page/PortfolioCaseStudy.jsx
@@ -1,24 +1,58 @@
-import React from "react";
-import { Pagination } from "@mui/material";
-
-const caseStudiesPortfolioConstants = [
-  {
-    id: 0,
-    imgSrc: "",
-    project: "",
-    customer: "",
-    tools: "",
-    database: "",
-    ETL_tools: "",
-    reportingAndVisualTools: "",
-    description: "",
-  },
-];
+import { useState } from "react";
+import { Paper, Box, Pagination, Typography } from "@mui/material";
+import { caseStudies } from "../../../constants/PortfolioConstants";
 
 const PortfolioCaseStudy = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const showCaseStudies = (
+    <Paper key={caseStudies[pageNumber].id} sx={{ margin: "40px" }}>
+      <Box>
+        <Typography>Project: {caseStudies[pageNumber].project}</Typography>
+        <img
+          src={caseStudies[pageNumber].imgSrc}
+          alt={caseStudies[pageNumber].project}
+        />
+      </Box>
+      <Box>
+        <Typography>Customer: {caseStudies[pageNumber].customer}</Typography>
+        {caseStudies[pageNumber].tools.length > 1 && (
+          <Typography>Tools: {caseStudies[pageNumber].tools}</Typography>
+        )}
+        {caseStudies[pageNumber].database > 1 && (
+          <Typography>Database: {caseStudies[pageNumber].database}</Typography>
+        )}
+        {caseStudies[pageNumber].ETL_tools > 1 && (
+          <Typography>
+            ETL Tools: {caseStudies[pageNumber].ETL_tools}
+          </Typography>
+        )}
+        {caseStudies[pageNumber].reportingAndVisualTools !== "" && (
+          <Typography>
+            Reporting and Visualization Tools:{" "}
+            {caseStudies[pageNumber].reportingAndVisualTools}
+          </Typography>
+        )}
+      </Box>
+      <Box>
+        <Typography>{caseStudies[pageNumber].description}</Typography>
+      </Box>
+    </Paper>
+  );
+
+  const handlePageIndex = (event, value) => {
+    setPageNumber(value - 1);
+  };
+
   return (
     <div>
-      <Pagination count={3} variant='outlined' color='error' shape='rounded' />
+      {showCaseStudies}
+      <Pagination
+        count={caseStudies.length}
+        variant='outlined'
+        color='error'
+        shape='rounded'
+        onChange={handlePageIndex}
+      />
     </div>
   );
 };
