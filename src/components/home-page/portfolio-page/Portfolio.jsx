929496780c81a7b9cbe2f1537c2b5e077c62d6c8
Shubham228197 <shubham228pandey@gmail.com>
2024-11-27 11:11:12 +0530

    whyPort RAW

--- src/components/home-page/portfolio-page/Portfolio.jsx
+++ src/components/home-page/portfolio-page/Portfolio.jsx
@@ -8,22 +8,57 @@ import PortfolioServices from "./PortfolioServices";
 
 const Portfolio = () => {
   const [activeTab, setActiveTab] = useState("about");
+  const [activeButton, setActiveButton] = useState({
+    about: true,
+    services: false,
+    case: false,
+  });
+
   const handleActiveTab = (newTab) => {
     setActiveTab(newTab);
+    switch (newTab) {
+      case "about":
+        setActiveButton({ about: true, services: false, case: false });
+        break;
+      case "services":
+        setActiveButton({ about: false, services: true, case: false });
+        break;
+      case "case":
+        setActiveButton({ about: false, services: false, case: true });
+        break;
+      default:
+        setActiveButton({ about: true, services: false, case: false });
+        break;
+    }
   };
   return (
     <>
       <Box>
-        <Button onClick={() => handleActiveTab("about")}>Aim</Button>
-        <Button onClick={() => handleActiveTab("services")}>Services</Button>
-        <Button onClick={() => handleActiveTab("case")}>Case Study</Button>
+        <Button
+          variant={activeButton.about ? "outlined" : "text"}
+          onClick={() => handleActiveTab("about")}
+        >
+          Aim
+        </Button>
+        <Button
+          variant={activeButton.services ? "outlined" : "text"}
+          onClick={() => handleActiveTab("services")}
+        >
+          Services
+        </Button>
+        <Button
+          variant={activeButton.case ? "outlined" : "text"}
+          onClick={() => handleActiveTab("case")}
+        >
+          Case Study
+        </Button>
       </Box>
       {activeTab === "about" && (
-        <Box>
+        <>
           <PortfolioAbout />
           <PortfolioWhyChooseUs />
           <PortfolioTechStack />
-        </Box>
+        </>
       )}
       {activeTab === "services" && <PortfolioServices />}
       {activeTab === "case" && <PortfolioCaseStudy />}
