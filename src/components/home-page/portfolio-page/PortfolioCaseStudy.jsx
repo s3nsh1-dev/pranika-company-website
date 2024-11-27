910e6047825a73faeadecce4afdfd89ccaa0eb6f
Shubham228197 <shubham228pandey@gmail.com>
2024-11-27 21:34:31 +0530

    custom Typo added

--- src/components/home-page/portfolio-page/PortfolioCaseStudy.jsx
+++ src/components/home-page/portfolio-page/PortfolioCaseStudy.jsx
@@ -1,40 +1,66 @@
 import { useState } from "react";
 import { Paper, Box, Pagination, Typography } from "@mui/material";
 import { caseStudies } from "../../../constants/PortfolioConstants";
+import {
+  PageTitleText,
+  PrimaryHeadingText,
+  SecondaryHeadingText,
+  BoldSpan,
+} from "../../common/TitleTexts";
 
 const PortfolioCaseStudy = () => {
   const [pageNumber, setPageNumber] = useState(0);
   const showCaseStudies = (
-    <Paper key={caseStudies[pageNumber].id} sx={{ margin: "40px" }}>
+    <Paper
+      key={caseStudies[pageNumber].id}
+      sx={{ margin: "3%", padding: "20px" }}
+      elevation={5}
+    >
       <Box>
-        <Typography>Project: {caseStudies[pageNumber].project}</Typography>
-        <img
+        <PrimaryHeadingText>
+          <BoldSpan>Project: </BoldSpan>
+          {caseStudies[pageNumber].project}
+        </PrimaryHeadingText>
+        {/* <img
           src={caseStudies[pageNumber].imgSrc}
           alt={caseStudies[pageNumber].project}
-        />
+        /> */}
       </Box>
-      <Box>
-        <Typography>Customer: {caseStudies[pageNumber].customer}</Typography>
+      <Box sx={{ margin: "30px 0px" }}>
+        <SecondaryHeadingText>
+          <BoldSpan>Customer: </BoldSpan>
+          {caseStudies[pageNumber].customer}
+        </SecondaryHeadingText>
         {caseStudies[pageNumber].tools.length > 1 && (
-          <Typography>Tools: {caseStudies[pageNumber].tools}</Typography>
+          <SecondaryHeadingText>
+            <BoldSpan>Tools: </BoldSpan>
+            {caseStudies[pageNumber].tools}
+          </SecondaryHeadingText>
         )}
         {caseStudies[pageNumber].database > 1 && (
-          <Typography>Database: {caseStudies[pageNumber].database}</Typography>
+          <SecondaryHeadingText>
+            <BoldSpan>Database: </BoldSpan>
+            {caseStudies[pageNumber].database}
+          </SecondaryHeadingText>
         )}
         {caseStudies[pageNumber].ETL_tools > 1 && (
-          <Typography>
-            ETL Tools: {caseStudies[pageNumber].ETL_tools}
-          </Typography>
+          <SecondaryHeadingText>
+            <BoldSpan>ETL Tools: </BoldSpan>
+            {caseStudies[pageNumber].ETL_tools}
+          </SecondaryHeadingText>
         )}
         {caseStudies[pageNumber].reportingAndVisualTools !== "" && (
-          <Typography>
-            Reporting and Visualization Tools:{" "}
+          <SecondaryHeadingText>
+            <BoldSpan>Reporting and Visualization Tools: </BoldSpan>
             {caseStudies[pageNumber].reportingAndVisualTools}
-          </Typography>
+          </SecondaryHeadingText>
         )}
       </Box>
       <Box>
-        <Typography>{caseStudies[pageNumber].description}</Typography>
+        <Typography>
+          <BoldSpan>Description: </BoldSpan>
+          {caseStudies[pageNumber].description}
+        </Typography>
       </Box>
     </Paper>
   );
@@ -44,7 +70,8 @@ const PortfolioCaseStudy = () => {
   };
 
   return (
-    <div>
+    <Box>
+      <PageTitleText>Our Success Stories</PageTitleText>
       {showCaseStudies}
       <Pagination
         count={caseStudies.length}
@@ -53,7 +80,7 @@ const PortfolioCaseStudy = () => {
         shape='rounded'
         onChange={handlePageIndex}
       />
-    </div>
+    </Box>
   );
 };
 
