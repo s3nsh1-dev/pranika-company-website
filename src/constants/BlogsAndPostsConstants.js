f27808f040534f562be38598897267e3fba1a5ef
Shubham228197 <shubham228pandey@gmail.com>
2024-11-30 21:08:00 +0530

    button labels

--- src/constants/BlogsAndPostsConstants.js
+++ src/constants/BlogsAndPostsConstants.js
@@ -191,3 +191,35 @@ export const textBasedContent = [
     ],
   },
 ];
+
+export const blogButtonLabels = [
+  {
+    id: 0,
+    buttonLabel: "Data Science & Business Analytics",
+    buttonKey: "DSBA",
+  },
+  { id: 1, buttonLabel: "AI & Machine Learning & Gen AI", buttonKey: "GenAI" },
+  { id: 2, buttonLabel: "Cloud Computing", buttonKey: "CC" },
+  {
+    id: 3,
+    buttonLabel: "DevOps & SRE (Site Reliability Engineering)",
+    buttonKey: "DevOps",
+  },
+  { id: 4, buttonLabel: "Software Development", buttonKey: "SDE" },
+  {
+    id: 5,
+    buttonLabel: "IT Service Management & Enterprise Architecture",
+    buttonKey: "ITSM",
+  },
+  {
+    id: 6,
+    buttonLabel: "Digital Marketing & Growth Hacking",
+    buttonKey: "DMGH",
+  },
+  { id: 7, buttonLabel: "Big Data and advance analytics", buttonKey: "BGAA" },
+  {
+    id: 8,
+    buttonLabel: "Career Fast-track & Professional Upskilling",
+    buttonKey: "Upskilling",
+  },
+];
