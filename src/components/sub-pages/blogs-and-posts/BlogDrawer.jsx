706a3324f3af3c0325fc9c1873cb7d65ed52d1f8
Shubham228197 <shubham228pandey@gmail.com>
2024-11-30 20:51:29 +0530

    comments

--- src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
+++ src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Divider } from "@mui/material";
 
 const styledContainer = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "start",
-  alignItems: "start",
   border: "2px solid black",
   width: "20%",
 };
@@ -13,18 +11,28 @@ const BlogDrawer = () => {
   return (
     <Box sx={styledContainer}>
       <Button>Data Science & Business Analytics</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>AI & Machine Learning & Gen AI</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>Cloud Computing</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>DevOps & SRE (Site Reliability Engineering)</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>Software Development</Button>
-      <Button sx={{ textAlign: "start" }}>
-        IT Service Management & Enterprise Architecture
-      </Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
+      <Button>IT Service Management & Enterprise Architecture</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>Digital Marketing & Growth Hacking</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>Big Data and advance analytics</Button>
+      <Divider orientation='horizontal' variant='middle' flexItem />
       <Button>Career Fast-track & Professional Upskilling</Button>
     </Box>
   );
 };
 
 export default BlogDrawer;
+/*
+see what this is ?: const activeBlog = blogs.find(blog => blog.key === activeKey);
+with the help of find we will find the first activeKey blog and show it content
+*/
