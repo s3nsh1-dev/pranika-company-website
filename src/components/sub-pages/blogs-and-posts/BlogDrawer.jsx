5420e6af78da854cfc5c1e9544bff043afa98049
Shubham228197 <shubham228pandey@gmail.com>
2024-11-30 21:08:11 +0530

    updates

--- src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
+++ src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
@@ -1,48 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Box, Button, Divider } from "@mui/material";
+import { blogButtonLabels } from "../../../constants/BlogsAndPostsConstants";
 
-const styledContainer = {
+export const styledContainer = {
   display: "flex",
   flexDirection: "column",
   border: "2px solid black",
   width: "20%",
 };
-const buttonsValue = [
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-  { id: 0, buttonName: "" },
-];
 const BlogDrawer = () => {
   // this will kep the track of key based on the buttons events
   const [activeKey, setActiveKey] = useState(null);
   return (
     <Box sx={styledContainer}>
-      <Button>Data Science & Business Analytics</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>AI & Machine Learning & Gen AI</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>Cloud Computing</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>DevOps & SRE (Site Reliability Engineering)</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>Software Development</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>IT Service Management & Enterprise Architecture</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>Digital Marketing & Growth Hacking</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>Big Data and advance analytics</Button>
-      <Divider orientation='horizontal' variant='middle' flexItem />
-      <Button>Career Fast-track & Professional Upskilling</Button>
+      {blogButtonLabels.map((button) => {
+        return (
+          <Fragment key={button.id}>
+            <Button
+              sx={{ textTransform: "none" }}
+              onClick={() => {
+                console.log(button.buttonKey);
+              }}
+            >
+              {button.buttonLabel}
+            </Button>
+            <Divider orientation='horizontal' variant='middle' flexItem />
+          </Fragment>
+        );
+      })}
     </Box>
   );
 };
