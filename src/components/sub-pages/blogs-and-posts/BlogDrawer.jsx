75f5d1076cff1286d416287fb53bd8e1289bfcce
Shubham228197 <shubham228pandey@gmail.com>
2024-11-30 21:41:20 +0530

    using Grid iguess

--- src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
+++ src/components/sub-pages/blogs-and-posts/BlogDrawer.jsx
@@ -1,9 +1,10 @@
 import React, { useState, Fragment } from "react";
-import { Box, Button, Divider } from "@mui/material";
+import { Box, Button, Divider, Grid } from "@mui/material";
 import {
   blogButtonLabels,
   blogDatabase,
 } from "../../../constants/BlogsAndPostsConstants";
+import TextBlogs from "./TextBlogs"; // send the active blog data to TextBlogs as props
 
 export const styledContainer = {
   display: "flex",
@@ -20,21 +21,28 @@ const BlogDrawer = () => {
   };
   console.log(activeKey);
   return (
-    <Box sx={styledContainer}>
-      {blogButtonLabels.map((button) => {
-        return (
-          <Fragment key={button.id}>
-            <Button
-              sx={{ textTransform: "none" }}
-              onClick={() => handleActiveKeyChange(button.buttonKey)}
-            >
-              {button.buttonLabel}
-            </Button>
-            <Divider orientation='horizontal' variant='middle' flexItem />
-          </Fragment>
-        );
-      })}
-    </Box>
+    <Grid container columns={12}>
+      {/* decide sx and md based on drawer proportion */}
+      <Grid item sx={styledContainer}>
+        {blogButtonLabels.map((button) => {
+          return (
+            <Fragment key={button.id}>
+              <Button
+                sx={{ textTransform: "none" }}
+                onClick={() => handleActiveKeyChange(button.buttonKey)}
+              >
+                {button.buttonLabel}
+              </Button>
+              <Divider orientation='horizontal' variant='middle' flexItem />
+            </Fragment>
+          );
+        })}
+      </Grid>
+      <Grid item>
+        {/* decide sx and md based on drawer proportion */}
+        <TextBlogs />
+      </Grid>
+    </Grid>
   );
 };
 
@@ -43,8 +51,11 @@ export default BlogDrawer;
 see what this is ?: const activeBlog = blogs.find(blog => blog.key === activeKey);
 with the help of find we will find the first activeKey blog and show it content
 
-Next Step: 
-create and fill blogdataBase with blog data
+Next Step:
+create and fill blogDataBase with blog data
 format it
 Then use this data to render blog post in BlogPost component
+
+const activeBlog = blogs.find(blog => blog.key === activeKey);
+will probably decide the blogDatabase data
 */
