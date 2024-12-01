c7d621a5ec2b2c18018cbb51b2ee6ed62693fa50
Shubham228197 <shubham228pandey@gmail.com>
2024-12-01 17:22:01 +0530

    custom button

---
+++ src/components/common/Buttons.jsx
@@ -0,0 +1,15 @@
+import { styled } from "@mui/material/styles";
+import { Button } from "@mui/material";
+
+export const BlueBorderButton = styled(Button)(({ theme }) => ({
+  textTransform: "none",
+  color: "#070066",
+  border: "2px solid #070066",
+}));
+export const BlueButton = styled(Button)(({ theme }) => ({
+  textTransform: "none",
+  color: "#070066",
+}));
+export const NormButton = styled(Button)(({ theme }) => ({
+  textTransform: "none",
+}));
