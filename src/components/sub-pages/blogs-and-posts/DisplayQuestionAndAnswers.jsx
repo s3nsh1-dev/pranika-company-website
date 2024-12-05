7b83333655630b5f94a4ff34aeb8b057a5083569
Shubham228197 <shubham228pandey@gmail.com>
2024-12-06 00:24:17 +0530

    no css comp done

---
+++ src/components/sub-pages/blogs-and-posts/DisplayQuestionAndAnswers.jsx
@@ -0,0 +1,45 @@
+import { Box, Typography } from "@mui/material";
+import { SecondaryHeadingText } from "../../common/TitleTexts";
+
+const DisplayQuestionAndAnswers = ({ questionBank }) => {
+  const answerAsObject = (object) => {
+    const content = [];
+    for (const [key, value] of Object.entries(object)) {
+      content.push(`${key}: ${value}`);
+    }
+    return (
+      <Box component='ol' type='a'>
+        {content.map((contentItem, index) => {
+          return (
+            <Typography key={contentItem} component='li'>
+              {contentItem}
+            </Typography>
+          );
+        })}
+      </Box>
+    );
+  };
+  const renderQuestionAndAnswers = questionBank.map((quesAns) => {
+    return (
+      <Box key={quesAns.id}>
+        <Box>
+          <SecondaryHeadingText fontWeight='bold'>
+            Question {quesAns.id}: {quesAns.question}
+          </SecondaryHeadingText>
+        </Box>
+        <Box component='details'>
+          <Box component='summary'>Answer</Box>
+          {typeof quesAns.answer === "string" ? (
+            <Typography>{quesAns.answer}</Typography>
+          ) : (
+            answerAsObject(quesAns.answer)
+          )}
+        </Box>
+      </Box>
+    );
+  });
+
+  return <Box>{renderQuestionAndAnswers}</Box>;
+};
+
+export default DisplayQuestionAndAnswers;
