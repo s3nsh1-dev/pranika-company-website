5db691e997fac3d4be349bbf1aad3858152823e4
Shubham228197 <shubham228pandey@gmail.com>
2024-12-06 23:29:30 +0530

    Q&A logic

--- src/components/sub-pages/blogs-and-posts/QuestionBankSets.jsx
+++ src/components/sub-pages/blogs-and-posts/QuestionBankSets.jsx
@@ -1,31 +1,74 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 import DisplayQuestionAndAnswers from "./DisplayQuestionAndAnswers";
-import { BlueButton } from "../../common/Buttons";
+import { BlueBorderButton } from "../../common/Buttons";
 
 const QuestionBankSets = ({ questionBankSets }) => {
-  const [questionBank, setQuestionBank] = useState([]);
-  const handleQuestionBankChange = (currentSet) => {
-    setQuestionBank(currentSet);
+  const [quesState, setQuesState] = useState({
+    prevButton: "",
+    questionBank: [],
+    selected: false,
+  });
+
+  const handleQuestionBankChange = ({ bankData, buttonId }) => {
+    if (quesState.selected === false) {
+      if (quesState.prevButton !== buttonId) {
+        setQuesState({
+          prevButton: buttonId,
+          questionBank: bankData,
+          selected: true,
+        });
+      }
+    } else if (
+      quesState.prevButton !== buttonId &&
+      quesState.selected === true
+    ) {
+      setQuesState({
+        prevButton: buttonId,
+        questionBank: bankData,
+        selected: true,
+      });
+    } else {
+      setQuesState({
+        prevButton: "",
+        questionBank: [],
+        selected: false,
+      });
+    }
   };
   const renderQuestionSets = questionBankSets.map((qSet) => {
     return (
       <Box key={qSet.topicId}>
-        <BlueButton
+        <BlueBorderButton
           onClick={() => {
-            handleQuestionBankChange(qSet.topicQuestionnaire);
+            handleQuestionBankChange({
+              bankData: qSet.topicQuestionnaire,
+              buttonId: qSet.topicId,
+            });
           }}
         >
           {qSet.topicTitle}
-        </BlueButton>
+        </BlueBorderButton>
       </Box>
     );
   });
   return (
-    <>
-      <Box>{renderQuestionSets}</Box>
-      <DisplayQuestionAndAnswers questionBank={questionBank} />
-    </>
+    <Box sx={{ display: "flex", flexDirection: "column", padding: "1%" }}>
+      <Box sx={{}}>{renderQuestionSets}</Box>
+      <Box sx={{ marginTop: "20px" }}>
+        {quesState.selected &&
+        quesState.prevButton !== "" &&
+        quesState.questionBank.length > 1 ? (
+          <DisplayQuestionAndAnswers questionBank={quesState.questionBank} />
+        ) : quesState.selected &&
+          quesState.prevButton !== "" &&
+          quesState.questionBank.length < 1 ? (
+          "Coming Soon..."
+        ) : (
+          " Select Your Topic for Question Bank"
+        )}
+      </Box>
+    </Box>
   );
 };
 
