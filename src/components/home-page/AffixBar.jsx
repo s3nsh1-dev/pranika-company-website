dd11455fd1510d25d0b24ece088b65e8b68f240a
Shubham228197 <shubham228pandey@gmail.com>
2024-12-12 20:40:26 +0530

    AffixBar color constant

--- src/components/home-page/AffixBar.jsx
+++ src/components/home-page/AffixBar.jsx
@@ -3,9 +3,7 @@ import {
   Toolbar,
   Button,
   Box,
-  Menu,
   MenuItem,
-  InputBase,
   IconButton,
   Drawer,
   List,
@@ -14,7 +12,6 @@ import {
   Divider,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
 import { Outlet, Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import Accordion from "@mui/material/Accordion";
@@ -26,64 +23,13 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-// import AffixbarMobileDrawer from "./AffixbarMobileDrawer";
-
-const StyledMenu = styled(Menu)(({ theme }) => ({
-  "& .MuiPaper-root": {
-    marginTop: "10px",
-    boxShadow: "none",
-    borderRadius: "none",
-  },
-}));
-
-const StyledSubMenu = styled(Menu)(({ theme }) => ({
-  "& .MuiPaper-root": {
-    marginTop: "-10px",
-    boxShadow: "none",
-    borderRadius: "none",
-  },
-}));
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import {
+  StyledMenu,
+  StyledSubMenu,
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../navbar-components/NavbarCssVariables";
 
 export default function AffixBar() {
   const [menuState, setMenuState] = useState({
