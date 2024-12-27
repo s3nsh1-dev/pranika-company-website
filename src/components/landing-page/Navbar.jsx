36fc1f8e98b7b9367bafe1547b99b173e187b162
s3nsh1-dev <shubham228pandey@gmail.com>
2024-12-27 18:13:45 +0530

    triming only

--- src/components/landing-page/Navbar.jsx
+++ src/components/landing-page/Navbar.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import { Outlet } from "react-router-dom";
-// import { AppBar, Box, Toolbar, useMediaQuery, useTheme } from "@mui/material";
-// import NavbarPrimaryContent from "./NavbarPrimaryContent";
-// import NavbarSecondaryContent from "./NavbarSecondaryContent";
-
-// export default function Navbar() {
-//   const theme = useTheme();
-//   const isSMScreen = useMediaQuery(theme.breakpoints.up("sm"));
-//   return (
-//     <>
-//       <Box>
-//         <AppBar
-//           position='static'
-//           sx={{
-//             backgroundColor: "#070066",
-//             margin: "0px",
-//             padding: "0px",
-//           }}
-//         >
-//           <Toolbar disableGutters>
-//             <NavbarPrimaryContent />
-//             {isSMScreen && <NavbarSecondaryContent />}
-//           </Toolbar>
-//         </AppBar>
-//         <Outlet />
-//       </Box>
-//     </>
-//   );
-// }
-// =======================================================================================
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { AppBar, Box, Toolbar, useMediaQuery, useTheme } from "@mui/material";
